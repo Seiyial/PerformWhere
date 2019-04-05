6617044abbb71f6c858283c28a501f5faac9fb94
Seiyial <seiyial@seiyianworks.io>
2019-04-06 03:15:08 +0800

    Made the server successfully render static

--- server/server.js
+++ server/server.js
@@ -4,36 +4,49 @@ const staticDir = path.resolve(__dirname, '../dist')
 const port = 8502
 
 const App = simples(port, {
-	static: { enabled: true, index: ['index.html'], location: '../dist' }
-}, (status) => console.log('simples started', status))
+	config: {
+		session: {
+			enabled: true,
+			preferred: 'gzip'
+		},
+		static: {
+			enabled: true,
+			index: ['index.html'],
+			location: staticDir
+		}
+	}
+})
+
+// App.router(path.join(__dirname, './routes.js'), {
+// })
 
 App.on('start', (conn) => {
 	console.log('(*) Server is up on Port', port, '!')
 })
 
 App.use((conn, next) => {
-	console.log(conn.method, conn.path, conn._router)
+	console.log(conn.method, conn.path)
 	
 	// Static File Middleware: Return static files if request is GET '/dist/<anything>'
-	if (conn.method === 'GET' && conn.path.startsWith('/dist/')) {
+	// if (conn.method === 'GET' && conn.path.startsWith('/dist/')) {
 
-		// get the request path and strip first 5 chars ('/dist/')
-		const reqPath = conn.path.substr(5)
+	// 	// get the request path and strip first 5 chars ('/dist/')
+	// 	const reqPath = conn.path.substr(5)
 
-		// absolute path of requested resource
-		const absPath = path.join(staticDir, reqPath)
-		console.log('ABSPATH', reqPath, absPath);
+	// 	// absolute path of requested resource
+	// 	const absPath = path.join(staticDir, reqPath)
+	// 	console.log('ABSPATH', reqPath, absPath);
 		
-		// check that absolute path is within static dir before sending content
-		// conn.drain(static(reqPath), next)
-	}
+	// 	// check that absolute path is within static dir before sending content
+	// 	// conn.drain(static(reqPath), next)
+	// }
 
 	next()
 })
 
-App.get('/', (conn) => {
-	conn.drain(static('/index.html'))
-})
+// App.get('/', (conn) => {
+// 	conn.drain(static('/index.html'))
+// })
 
 App.on('error', (conn) => {
 	console.log('(*) Error', conn)
